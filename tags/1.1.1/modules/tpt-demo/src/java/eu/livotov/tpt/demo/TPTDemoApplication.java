45f0e0edee4dc194e4d3cbb6970277a5876c51ef
livotov <livotov@29c4eaf6-4907-11de-9ab4-498524d4d007>
2011-02-21 11:33:58 +0000

    last pre 6.5 version, before separation of vdv library

---
+++ tags/1.1.1/modules/tpt-demo/src/java/eu/livotov/tpt/demo/TPTDemoApplication.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.livotov.tpt.demo;
+
+import eu.livotov.tpt.TPTApplication;
+import eu.livotov.tpt.demo.dialogs.DialogsDemoSection;
+import eu.livotov.tpt.demo.docview.DocumentViewerSection;
+import eu.livotov.tpt.demo.i18n.I18NSection;
+import eu.livotov.tpt.demo.widgets.WidgetsSection;
+import eu.livotov.tpt.demo.windows.WindowsSection;
+
+/**
+ *
+ * @author dll
+ */
+public class TPTDemoApplication extends TPTApplication
+{
+
+    @Override
+    public void applicationInit ()
+    {
+        setTheme ( "tptdemo" );
+
+        MainWindow mw = new MainWindow ();
+
+        mw.addDemoSection ( new DialogsDemoSection () );
+        mw.addDemoSection ( new WidgetsSection () );
+        mw.addDemoSection ( new WindowsSection () );
+        mw.addDemoSection ( new I18NSection () );
+        mw.addDemoSection ( new DocumentViewerSection() );
+
+
+        addWindow ( mw );
+    }
+
+    @Override
+    public void firstApplicationStartup ()
+    {
+    }
+}
